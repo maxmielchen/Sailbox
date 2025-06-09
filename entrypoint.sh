d3162a4450d5f3631266b77e6852d0cc5d9b0876
Max Mielchen <94404446+maxmielchen@users.noreply.github.com>
2025-06-09 13:51:09 +0000

    log funktion

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+log_info() {
+    echo "[ $(date '+%Y-%m-%d %H:%M:%S') ] [INFO] $1"
+}
+
 INIT="/var/init"
 SSHD_PID=""
 
 handle_stop() {
-    echo "[INFO] Container stop requested. Stopping SSH server..."
+    log_info "Container stop requested. Stopping SSH server..."
     if [ -n "$SSHD_PID" ] && kill -0 "$SSHD_PID" 2>/dev/null; then
         kill "$SSHD_PID"
         wait "$SSHD_PID"
-        echo "[INFO] SSH server stopped. Container will now exit."
+        log_info "SSH server stopped. Container will now exit."
     fi
     exit 0
 }
@@ -17,23 +21,23 @@ trap handle_stop SIGTERM SIGINT
 
 if [ ! -e $INIT ]; then
     touch $INIT
-    echo "-- Verify SSH Server --"
+    log_info "Verify SSH Server"
     ssh-keygen -A
-    echo "-- Add Docker --"
+    log_info "Add Docker socket permissions"
     chmod 666 /var/run/docker.sock
     chgrp docker /var/run/docker.sock
-    echo "-- Start SSH Server --"
+    log_info "Start SSH Server"
     /usr/sbin/sshd -D &
     SSHD_PID=$!
     wait $SSHD_PID
-    echo "-- Stopped SSH Server --"
+    log_info "Stopped SSH Server"
 else
-    echo "-- Add Docker --"
+    log_info "Add Docker socket permissions"
     chmod 666 /var/run/docker.sock
     chgrp docker /var/run/docker.sock
-    echo "-- Start SSH Server --"
+    log_info "Start SSH Server"
     /usr/sbin/sshd -D &
     SSHD_PID=$!
     wait $SSHD_PID
-    echo "-- Stopped SSH Server --"
+    log_info "Stopped SSH Server"
 fi
